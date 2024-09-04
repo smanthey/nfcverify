1e4dd4e46b84e5e2dd831a9489d0187e2b07305c
smanthey <scottman@me.com>
2024-09-04 00:48:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# List of valid NFC tag UIDs for verification
+# List of valid NFC tag UIDs
 valid_tag_uids = {
-    "04:63:1A:F5": "Verified Tag"  # Replace this with your NFC tag UID
+    "04:63:1A:F5": "Tag 1",
+    "04:63:XX:YY": "Tag 2"  # Add more tags here
 }
 
 @app.route('/nfc_verify')
 def nfc_verify():
-    uid = request.args.get('uid')  # Get the UID from the URL query parameter
+    uid = request.args.get('uid')  # Get UID from the query string
     
     if uid in valid_tag_uids:
         return f"NFC Tag Verified for {valid_tag_uids[uid]}"
